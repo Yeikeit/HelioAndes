83b691b2bfe6cf86a323523f7385c51aaa0d1d49
goCatrilef <go.catrilef@duocuc.cl>
2025-10-22 22:02:47 -0300

    Agragando las cards

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -1,10 +1,67 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+
 function Servicios() {
     return (
-        <div className='row mt'>
-            <div className='col-lg-12'>
-                <h2 className='mb-4'>Nuestras Soluciones</h2    >
+        <div className='row mt-7'>
+            <div className='col-lg-6'>
+                <h2 className='mb-4'>Servicios</h2 >
             </div>
+            <div className='col-lg-6'>
+                <h6>Estudio enérgetico, instalación certificada, monitoreo, mantención.</h6>
+            </div>
+            <div className='col-lg-3 mb-4'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className='col-lg-3 mb-4'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className='col-lg-3 mb-4'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className='col-lg-3 mb-4'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
         </div>
+        
     );
 }
 
