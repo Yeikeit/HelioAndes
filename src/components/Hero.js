5b9a00a1894b3c43a7feb205ee8bf5cc414e5bf4
PAAWSA02LC925 <Duoc@PAAWSA02LC925.duoc.acad>
2025-10-22 22:24:36 -0300

    Avance 1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,32 @@
+import { Col, Image, Row } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
+
 function Hero() {
     return (
-        <div></div>
+
+        <Container>
+            <Row>
+
+
+                <Col md={6}>
+                    <h1>Energía solar accesible y confiable para tu hogar o pyme</h1>
+                    <p>Dimensiona tu sistema, conoce le costo estimado y solicita asesoría en minutos. La DEMO te guía con valores referenciales.</p>
+
+                </Col>
+                <Col md={6}>
+                    <Card>
+                        <Card.Body>This is some text within a card body.</Card.Body>
+                    </Card>
+
+
+                </Col>
+
+
+            </Row>
+        </Container>
+
     );
 }
 
